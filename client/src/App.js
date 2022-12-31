6757cf52f8330c1157f24f8c2c6fd9a8b1966c20
Levi Arcane <zarcanist95@gmail.com>
2022-12-31 15:19:12 -0500

    Basic outline of frontpage complete with working CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import './normalize.css';
 import './App.css';
-
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import defaultUserImg from './assets/defaultUserImg.jpg';
+import chatGPTlogo from './assets/chatGPTlogo.jpg';
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <button className="sidemenu-button">
+          <FontAwesomeIcon className="plus-icon" icon={faPlus} />
+          <span> New Chat </span>
+        </button>
+      </aside>
+      <main className="chat-container">
+        <section className="chat--output-container">
+          <div className="chat--output-message-container">
+            <div className="center-message">
+              <div className="chat--output-userImg">
+                <img src={defaultUserImg} alt="user" />
+              </div>
+              <div className="chat--output-message"> This is the User </div>
+            </div>
+          </div>
+          <div className="chat--output-message-container chatgpt">
+            <div className="center-message">
+              <div className="chat--output-userImg">
+                <img src={chatGPTlogo} alt="user" />
+              </div>
+              <div className="chat--output-message"> This is the AI </div>
+            </div>
+          </div>
+        </section>
+        <section className="chat--input-container">
+          <input className="chat--input" type="text" placeholder="Type a message..." />
+        </section>
+      </main>
     </div>
   );
 }
